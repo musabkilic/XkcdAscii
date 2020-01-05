5177a618b5428ac34776c4bf024b2107535d4239
Musab Kılıç <musabkilic@protonmail.com>
2020-01-05 17:56:03 +0300

    first init

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 extern crate image;
 extern crate clap;
+extern crate reqwest;
+extern crate scraper;
+extern crate rss;
 
+use std::io;
+use std::io::Read;
+use scraper::Html;
+use scraper::Selector;
+use std::fs::File;
 use std::str::from_utf8;
 use std::path::Path;
 use clap::{Arg, App};
+
+
+const GET_RANDOM_COMIC_URL : &str = "https://c.xkcd.com/random/comic/";
+const GET_COMIC_URL : &str = "https://xkcd.com/";
+
 
 fn intensity_to_ascii(value: &u8) -> &str {
     // changes an intensity into an ascii character
@@ -12,10 +25,10 @@ fn intensity_to_ascii(value: &u8) -> &str {
         " ", ".", "^", ",", ":", "_", "=", "~", "+", "O", "o", "*",
         "#", "&", "%", "B", "@", "$"
     ];
-    
+
     let n_chars = ascii_chars.len() as u8;
     let step = 255u8 / n_chars;
-    for i in 1..(n_chars - 1) {   
+    for i in 1..(n_chars - 1) {
         let comp = &step * i;
         if value < &comp {
             let idx = (i - 1) as usize;
@@ -26,30 +39,66 @@ fn intensity_to_ascii(value: &u8) -> &str {
     ascii_chars[ (n_chars - 1) as usize ]
 }
 
-fn main() {
-    // setup clap argument parser for input file
-    let matches = App::new("asciify")
-                    .version("0.0.1")
-                    .author("edelsonc")
-                    .about("Turn an image into ascii art!")
-                    .args( &[
-                    Arg::from_usage("<INPUT> 'Sets the image file'"),
-                    Arg::from_usage("[resize] -s, --resize [width] [height] 'Rescale image; does not preserve aspect ratio'"),
-                    ])
-                    .get_matches();
+fn get_random_comic() -> String{
+    return get_comic(GET_RANDOM_COMIC_URL);
+}
 
-    // open image as new dynamic image
-    let image_name = matches.value_of("INPUT").unwrap();
+fn get_comic(comic_url:&str) -> String{
+    let mut res = reqwest::get(comic_url).unwrap();
+    let mut body = String::new();
+    res.read_to_string(&mut body).expect("[ERROR] Couldn't get data from the website!");
+
+    let fragment = Html::parse_fragment(&body);
+    let selector0 = Selector::parse("#comic").unwrap();
+    let selector1 = Selector::parse("img").unwrap();
+
+    for element0 in fragment.select(&selector0) {
+        for element1 in element0.select(&selector1){
+            let href = element1.value().attr("src");
+            match href{
+                None => (),
+                Some(x) => {
+                    return x.to_string()
+                },
+            }
+        }
+    }
+    panic!("[ERROR] Comic doesn't exist!");
+}
+
+fn get_comic_id(comic_id:&str) -> String{
+    return get_comic(&(GET_COMIC_URL.to_string()+comic_id+"/"));
+}
+
+fn save_comic(comic_image:String, image_name:&str){
+    let mut resp = reqwest::get(&("https:".to_string()+&comic_image)).expect("request failed");
+    let mut out = File::create(image_name).expect("failed to create file");
+    io::copy(&mut resp, &mut out).expect("failed to copy content");
+}
+
+fn main() {
+    let matches = App::new("asciify")
+                        .args( &[
+                        Arg::from_usage("[comic_id] -id, --comic_id [id] 'Comic ID'"),
+                        ])
+                        .get_matches();
+
+    let image = match matches.values_of_lossy("comic_id") {
+        Some(v) => get_comic_id(&v.join("")),
+        None => get_random_comic()
+    };
+    println!("{}", image);
+    let name_split = image.split(".").collect::<Vec<&str>>();
+    let image_name = &("comic.".to_owned()+name_split[name_split.len()-1]);
+    save_comic(image, image_name);
+
     let img = match image::open(&Path::new(image_name)) {
         Ok(p) => p,
-        Err(e) => panic!("Not a valid image path or could no open image"),
+        Err(_e) => panic!("Not a valid image path or could no open image"),
     };
 
     // resize image as an option if its very large...defualts to screen width
-    let dims = match matches.values_of_lossy("resize") {
-        Some(v) => v.iter().map(|s| s.parse::<u32>().unwrap()).collect(),
-        None => vec![80u32, 40u32],
-    };
+    let dims = vec![1280u32, 640u32];
 
     let img = img.resize_exact(dims[0], dims[1], image::FilterType::Nearest);
 
@@ -69,4 +118,3 @@ fn main() {
         println!("{}", s);
     }
 }
-
